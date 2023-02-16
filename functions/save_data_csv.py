d5a06509289f6399a48e2ad8008712abb9e04c40
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2023-02-16 15:01:43 -0300

    Fixed error with encoding

--- functions/save_data_csv.py
+++ functions/save_data_csv.py
@@ -1,14 +1,17 @@
 import sys
 import csv
+import os
 
 
 def save_data_csv(validators):
     try:
-        with open(f'./data/validators.csv', 'a') as file:
-            file.truncate(0)  # Delete file contents
+        file_path = './data/validators.csv'
+        file_exists = os.path.exists(file_path)
+        with open(file_path, 'a', encoding="utf-8") as file:
             writer = csv.writer(file)
-            writer.writerow(
-                ["index", "pool", "validator_address", "depositor_address"])
+            if not file_exists:
+                writer.writerow(
+                    ["index", "pool", "validator_address", "depositor_address"])
             for validator in validators:
                 writer.writerow(
                     [validator["index"], validator["pool"], validator["validator_address"], validator["depositor_address"]])
