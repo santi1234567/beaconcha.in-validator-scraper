a77399123ef0fbf0f467712b2588518d5231d1ae
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2023-02-16 11:16:57 -0300

    Changed function to store all validators together

--- functions/save_data_csv.py
+++ functions/save_data_csv.py
@@ -1,23 +1,21 @@
 import sys
 import csv
-import json
 
 
 def save_data_csv(validators):
-    for pool in validators:
-        try:
-            with open(f'./data/{pool}.csv', 'a') as file:
-                file.truncate(0)  # Delete file contents
-                writer = csv.writer(file)
+    try:
+        with open(f'./data/validators.csv', 'a') as file:
+            file.truncate(0)  # Delete file contents
+            writer = csv.writer(file)
+            writer.writerow(
+                ["index", "pool", "validator_address", "depositor_address"])
+            for validator in validators:
                 writer.writerow(
-                    ["index", "validator_address", "depositor_address"])
-                for validator in validators[pool]:
-                    writer.writerow(
-                        [validator["index"], validator["validator_address"], validator["depositor_address"]])
-                return True
-        except NameError:
-            print(NameError)
-            return False
+                    [validator["index"], validator["pool"], validator["validator_address"], validator["depositor_address"]])
+            return True
+    except NameError:
+        print(NameError)
+        return False
 
 
 sys.modules[__name__] = save_data_csv
